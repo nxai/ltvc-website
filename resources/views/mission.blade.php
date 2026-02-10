f95196d403129361712dcaebb903c6be470990fe
Kittisay SENGTHONG <85366173+Kittisayst@users.noreply.github.com>
2026-02-10 14:47:37 +0700

    fix: route & add page mission

---
+++ resources/views/mission.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+    <h1>Mission</h1>
+    <p>Let all your things have their places; let each part of your business have its time. - Benjamin Franklin</p>
+</div>
